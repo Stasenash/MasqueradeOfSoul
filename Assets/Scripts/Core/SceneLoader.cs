6069bb307e992c3a961d313f61c91f97891bf5f8
Stasenash <staslisenash@gmail.com>
2026-01-27 15:40:00 +0500

    Try Moving System

---
+++ Assets/Scripts/Core/SceneLoader.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+   public static SceneLoader Instance;
+
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 3f;
+
+    private Rigidbody2D rb;
+    private float inputX;
+    private Vector3 baseScale;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        baseScale = transform.localScale;
+    }
+
+    void Update()
+    {
+        inputX = Input.GetAxisRaw("Horizontal");
+    }
+
+    void FixedUpdate()
+    {
+        rb.velocity = new Vector2(inputX * speed, 0f);
+
+        if (inputX != 0)
+        {
+            transform.localScale = new Vector3(
+                baseScale.x * Mathf.Sign(inputX),
+                baseScale.y,
+                baseScale.z
+            );
+        }
+    }
+}
