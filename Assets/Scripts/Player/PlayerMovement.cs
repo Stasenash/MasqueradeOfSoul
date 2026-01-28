d63309f773470477f0bb12386a047a0eacb9d35b
Stasenash <staslisenash@gmail.com>
2026-01-28 18:22:11 +0500

    Animation added

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -4,29 +4,32 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float speed = 3f;
+    [SerializeField] private float speed = 3f;
 
+    private Animator animator;
     private Rigidbody2D rb;
     private float inputX;
     private Vector3 baseScale;
 
     void Awake()
     {
+        animator = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
         baseScale = transform.localScale;
     }
 
     void Update()
     {
-        if (Time.timeScale == 0f) return;
+        if (Time.timeScale == 0f)
+        {
+            animator.SetFloat("Speed", 0f);
+            return;
+        }
+
         inputX = Input.GetAxisRaw("Horizontal");
-    }
 
-    void FixedUpdate()
-    {
-        rb.velocity = new Vector2(inputX * speed, 0f);
-
-        if (inputX != 0)
+        // Разворот
+        if (Mathf.Abs(inputX) > 0.01f)
         {
             transform.localScale = new Vector3(
                 baseScale.x * Mathf.Sign(inputX),
@@ -34,5 +37,13 @@ public class PlayerMovement : MonoBehaviour
                 baseScale.z
             );
         }
+
+        // Анимация: используем РЕАЛЬНОЕ движение
+        animator.SetFloat("Speed", Mathf.Abs(rb.velocity.x));
+    }
+
+    void FixedUpdate()
+    {
+        rb.velocity = new Vector2(inputX * speed, 0f);
     }
 }
